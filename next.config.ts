98bd8c81798b3361e3e4cf225dd672906e2442fc
Patrick Klly <doitformurphy@gmail.com>
2025-02-15 23:32:12 -0500

    Update dependencies and integrate Nextra for enhanced documentation support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+import nextra from 'nextra'
+ 
+const withNextra = nextra({
+  // ... Other Nextra config options
+})
+ 
+// You can include other Next.js configuration options here, in addition to Nextra settings:
+export default withNextra({
+  // ... Other Next.js config options
+})
